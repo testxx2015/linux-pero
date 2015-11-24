c093672387fa2c7246e35ed23c2c18e99db8c317
Darko Stimac <Darko.Stimac@logicbricks.com>
2015-11-24 16:01:31 +0100

    xylon-v2014.4-trd
        Added clk-logiclk v1.0
        Added xylonfb v3.0
        Added xylonfg v1.0
        Added xyloni2s v1.0
        Added adv7611 misc I2C driver
        Added drivers binding documentation.
        Added Xylon Referent Design on HDMI output defconfig and dts file for ZED board.

--- main.c
+++ main.c
@@ -3,4 +3,9 @@ void main()
 	int i;
 	i++;	//pero v1.0
 	i++;	//pero v1.2
+	i+=100; //developer 17:16 23.11.2015
+	i+=200; //developer 17:20 23.11.2015
+	i+=300; //developer  9:43 24.11.2015
+	i+=301; //developer  9:46 24.11.2015
 }
+
